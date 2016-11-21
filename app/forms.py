9e95ce4810a0ffb9e812402b09fddcefa2b130f7
humblehound <dejtabejz@gmail.com>
2016-11-21 23:25:23 +0100

    One in all configuration

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask import flash
-from flask_wtf import Form
-from wtforms import StringField, FileField
-from wtforms.validators import DataRequired, ValidationError
-import validators
-
-
-class SearchForm(Form):
-    file = FileField('file', validators=[DataRequired()])
--- config.py
+++
@@ -1,2 +0,0 @@
-CSRF_ENABLED = False
-SECRET_KEY = 'you-will-never-guess'
